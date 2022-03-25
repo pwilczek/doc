f3da8cea70437c426050cc70f1d634b04cef89e3
Piotr Wilczek <piotr.wilczek@wilu.name>
2022-03-25 08:14:23 +0100

    initial commit

---
+++ src/test/java/name/wilu/doc/DocApplicationTests.java
@@ -0,0 +1,13 @@
+package name.wilu.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
